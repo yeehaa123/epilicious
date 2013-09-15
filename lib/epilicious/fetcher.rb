623a524b2c9046c3284dbbef0b2a41ba16044770
Yeehaa <medialoperations@gmail.com>
2013-09-15 13:27:42 +0200

    get list of recipe url

--- lib/epilicious/fetcher.rb
+++ lib/epilicious/fetcher.rb
@@ -1,24 +1,29 @@
 require 'open-uri'
 require 'nokogiri'
+require 'pry'
 
 module Epilicious
 
   class Fetcher
-    attr_reader :recipe_urls
+    attr_accessor :recipe_urls
 
     def initialize
       @recipe_urls ||= []
     end
 
     def fetch_recipes(url = default_url)
-      fetch_recipes_index(url)
-      recipe_urls << 'test' 
+      recipes_page = fetch_recipes_page(url)
+      self.recipe_urls = parse_recipes_page(recipes_page)
     end
     
     private
 
-    def fetch_recipes_index(url)
+    def fetch_recipes_page(url)
       Nokogiri::HTML(open(url))
+    end
+
+    def parse_recipes_page(page)
+      page.css('#artInner .recipe_result_right a').map {|link| link ['href'] }
     end
 
     def default_url
--- test/fetcher_test.rb
+++ test/fetcher_test.rb
@@ -2,7 +2,6 @@ require 'test_helper'
 
 describe Epilicious::Fetcher do
   let(:fetcher) { Epilicious::Fetcher.new }
-  let(:url) { "http://www.epicurious.com/articlesguides/bestof/toprecipes/bestburgerrecipes" }
 
   context '#fetch_recipes' do
     it 'should return a array' do
@@ -13,29 +12,21 @@ describe Epilicious::Fetcher do
       fetcher.fetch_recipes.wont_be_empty
     end
 
-    it 'should contain recipes' do
+    it 'should contain urls' do
       fetcher.fetch_recipes.each do |recipe|
         recipe.must_be_instance_of String
       end
     end
-
-    it 'should call fetch_recipes_index with a default url' do
-
-      fetcher.expects(:fetch_recipes_index).with(url)
-      fetcher.fetch_recipes
-    end
-
-    it 'should call fetch_recipes_index with a custom url' do
-      custom = "http://www.epicurious.com/articlesguides/bestof/toprecipes/bestpastarecipes"
-
-      fetcher.expects(:fetch_recipes_index).with(custom)
-      fetcher.fetch_recipes(custom)
-    end
   end
 
-  context '#fetch_recipes_index' do
-    it 'should return a nokogiri document' do
-      fetcher.send(:fetch_recipes_index, url).must_be_instance_of Nokogiri::HTML::Document
+  context 'private methods' do
+    let(:recipes_page) { fetcher.send(:fetch_recipes_page, url) }
+    let(:url) { "http://www.epicurious.com/articlesguides/bestof/toprecipes/bestburgerrecipes" }
+                        
+    context '#fetch_recipes_page' do
+      it 'should return a nokogiri document' do
+        recipes_page.must_be_instance_of Nokogiri::HTML::Document
+      end
     end
   end
 end
