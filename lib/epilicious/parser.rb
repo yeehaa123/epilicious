b56910c0f87dafc479060e7c57052ce0c71e05e2
Yeehaa <medialoperations@gmail.com>
2013-09-15 17:15:35 +0200

    parse individual recipes

--- lib/epilicious/parser.rb
+++ lib/epilicious/parser.rb
@@ -7,8 +7,31 @@ module Epilicious
 
     def parse_recipe_page(page)
       recipe = {}
-      recipe[:name] = page.css("div#headline h1").text 
+      recipe[:name]        = name(page) 
+      recipe[:servings]    = servings(page) 
+      recipe[:ingredients] = ingredients(page) 
+      recipe[:instructions] = instructions(page) 
       recipe
+    end
+
+    private
+    
+    def name(page)
+      page.css("div#headline h1").text 
+    end
+
+    def servings(page)
+      page.css("div#recipe_summary p span").first.text 
+    end
+
+    def ingredients(page)
+      ingredients_list = page.css("ul.ingredientsList li")
+      ingredients_list.map {|li| li.text}
+    end
+
+    def instructions(page)
+      preparation = page.css("div#preparation.instructions p")
+      preparation.map {|i| i.text}
     end
   end
 end
--- test/parser_test.rb
+++ test/parser_test.rb
@@ -24,7 +24,30 @@ describe Epilicious::Parser do
     end
   end
 
-  context '#parse_recipes_page' do
+  context '#parse_recipe_page' do
+    let(:recipe) { parser.send(:parse_recipe_page, recipe_page) }
+
+    it 'should have a name' do
+      recipe[:name].must_equal "Grilled Turkey Burgers with Cheddar and Smoky Aioli"  
+    end
+
+    it 'should tell the number of servings' do
+      recipe[:servings].must_equal "Makes 4"
+    end
+
+    it 'should have a list of ingredients' do
+      recipe[:ingredients].length.must_equal 15
+      recipe[:ingredients].first.must_equal "1/2 teaspoon cumin seeds"
+    end
+
+    it 'should have a list of instructions' do
+      recipe[:instructions].length.must_equal 6
+    end
+
+    def recipe_page
+      url = "/articlesguides/bestof/toprecipes/bestburgerrecipes/recipes/food/views/Grilled-Turkey-Burgers-with-Cheddar-and-Smoky-Aioli-354289"
+      Epilicious::Fetcher.new.send(:fetch_page, url)
+    end
   end
 
   def parser
